db1c29cfab844d4934089ba2270571e617bd02ff
loubarb <louis.barbati@gmail.com>
2022-04-06 23:53:05 -0400

    Added function for current date and divs for weather info

--- src/App.js
+++ src/App.js
@@ -6,11 +6,34 @@ const api = {
 }
 
 function App() {
+  const dateBuilder = (d) => {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  }
   return (
-    <div className="App warm">
+    <div className="App">
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." />
+        </div>
+        <div className="location-box">
+          <div className="location">NYC, US</div>
+          <div className="date">{dateBuilder(new Date())}</div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">
+            45°F
+          </div>
+          <div className="weather">
+            Cloudy
+          </div>
         </div>
       </main>
     </div>
