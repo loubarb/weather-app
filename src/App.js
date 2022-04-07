a6189b3101ed7ffaee50487b8127157ea60ebfe6
loubarb <louis.barbati@gmail.com>
2022-04-07 01:37:46 -0400

    Plugged in data from users input city to display and change background on weather type

--- src/App.js
+++ src/App.js
@@ -25,23 +25,31 @@ function App() {
   const currentDate = getDate.toDateString();
 
   return (
-    <div className="App">
+    <div className={(typeof weather.main != "undefined") 
+      ? ((weather.main.temp > 60) 
+        ? 'app warm' 
+        : 'app') 
+      : 'app'}>
       <main>
         <div className="search-box">
           <input type="text" className="search-bar" placeholder="Search..." onChange={e => setQuery(e.target.value)} value={query} onKeyPress={search} />
         </div>
-        <div className="location-box">
-          <div className="location">NYC, US</div>
-          <div className="date">{currentDate}</div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">
-            45°F
+        {(typeof weather.main != "undefined") ? (
+        <div>
+          <div className="location-box">
+            <div className="location">{weather.name}, {weather.sys.country}</div>
+            <div className="date">{currentDate}</div>
           </div>
-          <div className="weather">
-            Cloudy
+          <div className="weather-box">
+            <div className="temp">
+              {Math.round(weather.main.temp)}°F
+            </div>
+            <div className="weather">
+              {weather.weather[0].main}
+            </div>
           </div>
         </div>
+        ) : ('')}
       </main>
     </div>
   );
